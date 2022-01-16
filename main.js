1d812397885cee3370ad1b8e77ed8352476b1701
Matheus Aurélio <matheusaurelio2004@gmail.com>
2022-01-16 17:02:18 -0300

    Definindo posição da cobrinha ao ultrapassar o canvas

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let canvas = document.querySelector("canvas")
+let canvas = document.querySelector("#snake")
 let context = canvas.getContext("2d")
 let box = 32
 
@@ -19,7 +19,7 @@ function createBackgroundGame() {
 }
 
 function createSnake() {
-    for (let i = 0; i < snake.length; i++) {
+    for (let i in snake) {
         context.fillStyle = "green"
         context.fillRect(snake[i].x, snake[i].y, box, box)
     }
@@ -35,10 +35,14 @@ function update(event) {
     if (event.key == "ArrowLeft" && direction != "right") direction = "left"
 
     if (event.key == "ArrowRight" && direction != "left") direction = "right"
-
 }
 
 function startGame() {
+    if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0
+    if (snake[0].x < 0 && direction == "left") snake[0].x = 16 * box
+    if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0
+    if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box
+
     createBackgroundGame()
     createSnake()
 
@@ -47,8 +51,8 @@ function startGame() {
 
     if (direction == "right") snakeX += box
     if (direction == "left") snakeX -= box
-    if (direction == "up") snakeY += box
-    if (direction == "down") snakeY -= box
+    if (direction == "up") snakeY -= box
+    if (direction == "down") snakeY += box
 
     snake.pop()
 
