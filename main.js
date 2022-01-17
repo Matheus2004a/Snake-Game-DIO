516b091490f72a9fa33b2f6854387f85fa5ead95
Matheus Aurélio <matheusaurelio2004@gmail.com>
2022-01-16 23:23:35 -0300

    Alterando posição das frutinhas e centralizando canvas

--- main.js
+++ main.js
@@ -2,9 +2,6 @@ let canvas = document.querySelector("#snake")
 let context = canvas.getContext("2d")
 let box = 32
 
-canvas.width = window.screen.width
-canvas.height = window.screen.height
-
 let snake = []
 snake[0] = {
     x: 8 * box,
@@ -14,13 +11,8 @@ snake[0] = {
 let direction = "right"
 
 let food = {
-    x: Math.floor(Math.random() * 16) + box,
-    y: Math.floor(Math.random() * 16) + box,
-}
-
-function drawFood() {
-    context.fillStyle = "red"
-    context.fillRect(food.x, food.y, box, box)
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
 }
 
 function createBackgroundGame() {
@@ -29,21 +21,23 @@ function createBackgroundGame() {
 }
 
 function createSnake() {
-    for (let i in snake) {
+    for (let i = 0; i < snake.length; i++) {
         context.fillStyle = "green"
         context.fillRect(snake[i].x, snake[i].y, box, box)
     }
+}
+
+function drawFood() {
+    context.fillStyle = "red"
+    context.fillRect(food.x, food.y, box, box)
 }
 
 document.addEventListener("keydown", update)
 
 function update(event) {
     if (event.key == "ArrowUp" && direction != "down") direction = "up"
-
     if (event.key == "ArrowDown" && direction != "up") direction = "down"
-
     if (event.key == "ArrowLeft" && direction != "right") direction = "left"
-
     if (event.key == "ArrowRight" && direction != "left") direction = "right"
 }
 
@@ -52,6 +46,13 @@ function startGame() {
     if (snake[0].x < 0 && direction == "left") snake[0].x = 16 * box
     if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0
     if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box
+
+    for (let i = 1; i < snake.length; i++) {
+        if (snakeX[0].x == snake[i].x && snakeX[0].y == snake[i].y) {
+            clearInterval(game)
+            alert("Game over :(")
+        }
+    }
 
     createBackgroundGame()
     createSnake()
@@ -65,7 +66,12 @@ function startGame() {
     if (direction == "up") snakeY -= box
     if (direction == "down") snakeY += box
 
-    snake.pop()
+    if (snakeX != food.x || snakeY != food.y) {
+        snake.pop()
+    } else {
+        food.x = Math.floor(Math.random() * 15 + 1) * box
+        food.y = Math.floor(Math.random() * 15 + 1) * box
+    }
 
     let newHead = {
         x: snakeX,
