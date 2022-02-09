96556775fc385d180c1877cf819027529ffff420
Matheus Aurélio <matheusaurelio2004@gmail.com>
2022-02-09 14:30:39 -0300

    Centralizando gráfico

--- main.js
+++ main.js
@@ -35,10 +35,10 @@ function drawFood() {
 document.addEventListener("keydown", update)
 
 function update(event) {
-    if(event.keyCode == 37 && direction != 'right') direction = 'left';
-    if(event.keyCode == 38 && direction != 'down') direction = 'up';
-    if(event.keyCode == 39 && direction != 'left') direction = 'right';
-    if(event.keyCode == 40 && direction != 'up') direction = 'down';
+    if(event.keyCode == 37 && direction !== 'right') direction = 'left';
+    if(event.keyCode == 38 && direction !== 'down') direction = 'up';
+    if(event.keyCode == 39 && direction !== 'left') direction = 'right';
+    if(event.keyCode == 40 && direction !== 'up') direction = 'down';
 }
 
 function startGame() {
@@ -48,7 +48,7 @@ function startGame() {
     if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
 
     for (let i = 1; i < snake.length; i++) {
-        if (snakeX[0].x == snake[i].x && snakeX[0].y == snake[i].y) {
+        if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
             clearInterval(game);
             alert("Game over :(");
         }
