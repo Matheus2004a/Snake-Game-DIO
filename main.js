5062592760a8e58a9101e37604a37d1dac180938
Matheus Aurélio <matheusaurelio2004@gmail.com>
2022-03-06 22:08:40 -0300

    Adicionando aúdio

--- main.js
+++ main.js
@@ -35,10 +35,10 @@ function drawFood() {
 document.addEventListener("keydown", update)
 
 function update(event) {
-    if(event.keyCode == 37 && direction !== 'right') direction = 'left';
-    if(event.keyCode == 38 && direction !== 'down') direction = 'up';
-    if(event.keyCode == 39 && direction !== 'left') direction = 'right';
-    if(event.keyCode == 40 && direction !== 'up') direction = 'down';
+    if (event.keyCode == 37 && direction !== 'right') direction = 'left';
+    if (event.keyCode == 38 && direction !== 'down') direction = 'up';
+    if (event.keyCode == 39 && direction !== 'left') direction = 'right';
+    if (event.keyCode == 40 && direction !== 'up') direction = 'down';
 }
 
 function startGame() {
@@ -82,3 +82,21 @@ function startGame() {
 }
 
 let game = setInterval(startGame, 100);
+
+const iconSound = document.querySelector(".sound")
+iconSound.addEventListener("click", activeSound)
+
+function activeSound() {
+    let isWithSound = iconSound.classList.contains("sound")
+    
+    const insertAudio = document.querySelector("audio")
+    insertAudio.play()
+
+    if (isWithSound) {
+        iconSound.className = "fa-solid fa-volume-xmark"
+        insertAudio.pause()
+    } else {
+        iconSound.className = "fa-solid fa-volume-high sound"
+        insertAudio.play()
+    }
+}
